e13b11407d82f229a3408bfba51b31b78e5ce895
AlexRP <alexrubioplana14@gmail.com>
2024-11-02 21:18:42 +0100

    Movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float speed;
-
     private Vector2 inputMovementDirection;
     private Vector3 movementDirection;
+
+    [Header("HP")]
+    [SerializeField] private float hp;
+    private float currentHp;
+
+    [Header("Dash")]
+    [SerializeField] private float dashForce;
+    private Vector3 dashDirection;
+
+    
 
     private Rigidbody rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        currentHp = hp;
     }
 
 
@@ -28,6 +40,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        if(Input.GetKeyDown(KeyCode.L)) { ReceiveDamage(20); }
     }
 
     private void Move()
@@ -36,9 +49,36 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movementDirection * speed * Time.deltaTime, ForceMode.Force);
     }
 
+    private void ReceiveDamage(float damage)
+    {
+        currentHp -= damage;
+        CheckIfDead();
+        Debug.Log(currentHp);
+    }
+
+    private void CheckIfDead()
+    {
+        if(currentHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
     public void MovementAction(InputAction.CallbackContext obj)
     {
         inputMovementDirection = obj.action.ReadValue<Vector2>();
-        Debug.Log(inputMovementDirection);
+    }
+
+    public void DashAction(InputAction.CallbackContext obj)
+    {
+        if(inputMovementDirection != Vector2.zero)
+            dashDirection = new Vector3(inputMovementDirection.x, 0, inputMovementDirection.y);
+
+        rb.AddForce(dashDirection * dashForce * Time.deltaTime, ForceMode.Impulse);
     }
 }
