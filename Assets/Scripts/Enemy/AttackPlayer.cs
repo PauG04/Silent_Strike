1eec97bd9437e8f256758e7dde17a2fc280660ad
Alexrubio14 <alejandro.rubio@enti.cat>
2024-11-07 15:34:16 +0100

    Stamina bar done & invencibility while dashing

--- Assets/Scripts/Enemy/AttackPlayer.cs
+++ Assets/Scripts/Enemy/AttackPlayer.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class AttackPlayer : MonoBehaviour
 {
@@ -9,21 +6,8 @@ public class AttackPlayer : MonoBehaviour
     [SerializeField] private ParticleSystem[] slashParticles;
     [SerializeField] private float knockBackForce;
     [SerializeField] private GameObject blood;
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player") && enemy.GetCurrentState() == Enemy.enemyState.ATTACK && !enemy.GetAttackHitted() && enemy.GetCanAttack())
-        {
-            enemy.SetAttackHitted(true);
-            slashParticles[0].gameObject.transform.parent.transform.position = other.transform.position;
-            for(int i = 0; i<slashParticles.Length; i++) 
-            {
-                slashParticles[i].Play();
-            }
-            other.GetComponent<PlayerController>().ReceiveDamage(enemy.GetDamage());
-            other.GetComponent<Rigidbody>().AddForce((other.transform.position - gameObject.transform.parent.transform.position).normalized * knockBackForce, ForceMode.Impulse);
-            GenerateBlood(other.gameObject);
-        }
-    }
+
+    [SerializeField] private PlayerController playerController;
 
     private void GenerateBlood(GameObject player)
     {
@@ -32,4 +16,29 @@ public class AttackPlayer : MonoBehaviour
         _blood.GetComponent<SpriteRenderer>().sortingOrder = player.GetComponent<SpriteRenderer>().sortingOrder + 1;
         _blood.GetComponent<Rigidbody>().AddForce((player.transform.position - gameObject.transform.parent.transform.position).normalized * knockBackForce * 6, ForceMode.Impulse);
     }
+
+    private void PlayerHit(Collider other)
+    {
+        enemy.SetAttackHitted(true);
+        slashParticles[0].gameObject.transform.parent.transform.position = other.transform.position;
+        for (int i = 0; i < slashParticles.Length; i++)
+        {
+            slashParticles[i].Play();
+        }
+        other.GetComponent<PlayerController>().ReceiveDamage(enemy.GetDamage());
+        other.GetComponent<Rigidbody>().AddForce((other.transform.position - gameObject.transform.parent.transform.position).normalized * knockBackForce, ForceMode.Impulse);
+        GenerateBlood(other.gameObject);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && enemy.GetCurrentState() == Enemy.enemyState.ATTACK && !enemy.GetAttackHitted() && enemy.GetCanAttack())
+        {
+            if (playerController.GetState() == PlayerController.State.DASHING)
+                return;
+
+            PlayerHit(other);
+        }
+    }
+
+   
 }
